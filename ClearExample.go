db46e7d570cf58fa00fe3844c35a98194d8c71fe
Yuan-Heng,Kuo <coco8802231225@gmail.com>
2023-11-25 15:04:46 +0800

    add the original code

--- ClearExample.go
+++ ClearExample.go
@@ -22,4 +22,34 @@ func main() {
 
 	fmt.Println("After clear:", myMap)
 	// Result: After clear: map[]
+
+	// The Original Method
+	s := []int{1, 2, 3, 4, 5}
+	fmt.Println("Before clear:", s)
+	// s = s[:0]
+	s = nil
+	fmt.Println("After clear:", s)
+
+	m := map[string]int{"a": 1, "b": 2, "c": 3}
+	fmt.Println("Before clear:", m)
+	// for k := range m {
+	// 	delete(m, k)
+	// }
+	m = nil
+	fmt.Println("After clear:", m)
+
+	s := []int{1, 2, 3, 4, 5}
+	// Approach 1
+	s = s[:0]
+	// Approach 2
+	s = nil
+
+	m := map[string]int{"a": 1, "b": 2, "c": 3}
+
+	// Approach 1
+	for k := range m {
+		delete(m, k)
+	}
+	// Approach 2
+	m = nil
 }
